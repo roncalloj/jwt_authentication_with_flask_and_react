e00647305449bd1d6a2bef0e4284681aacdf806d
roncalloj <r-callo@hotmail.com>
2022-12-06 15:01:23 -0500

    new commit

--- src/api/models.py
+++ src/api/models.py
@@ -2,33 +2,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	email = db.Column(db.String(120), unique=True, nullable=False)
-	password = db.Column(db.String(300), unique=False, nullable=False)
-	is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-	
-	def __repr__(self):
-		return f'<User {self.email}>'
-	
-	def serialize(self):
-		return {
-			"id": self.id,
-			"email": self.email,
-			# do not serialize the password, its a security breach
-		}
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(300), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.email}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
 
 class TokenBlockedList(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	token = db.Column(db.String(1000), unique=True, nullable=False)
-	email = db.Column(db.String(200), nullable=False, index=True)
-	created_at = db.Column(db.DateTime, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(1000), unique=True, nullable=False)
+    email = db.Column(db.String(200), nullable=False, index=True)
+    created_at = db.Column(db.DateTime, nullable=False)
 
-	def serialize(self):
-		return {
-			"id":self.id,
-			"token":self.token,
-			"email":self.email,
-			"created_at":self.created_at
-		}
-
+    def serialize(self):
+        return {"id": self.id, "token": self.token, "email": self.email, "created_at": self.created_at}
--- src/app.py
+++ src/app.py
@@ -12,44 +12,54 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
-from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, get_jwt_identity, get_jwt
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity,
+    get_jwt,
+)
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from datetime import timedelta
 
-#from models import Person
+# from models import Person
 
 ENV = os.getenv("FLASK_ENV")
-static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../public/")
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-#Configuracion de JWT
+# Configuracion de JWT
 app.config["JWT_SECRET_KEY"] = os.getenv("FLASK_APP_KEY")
-#app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_HOURS")))
-#app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=int(os.getenv("JWT_REFRESH_TOKEN_EXPIRES_DAYS")))
+# app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_HOURS")))
+# app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=int(os.getenv("JWT_REFRESH_TOKEN_EXPIRES_DAYS")))
 jwt = JWTManager(app)
+
+
 @jwt.token_in_blocklist_loader
-def check_blocked_tokens(jwt_header,jwt_payload) -> bool:
+def check_blocked_tokens(jwt_header, jwt_payload) -> bool:
     tokenBlocked = TokenBlockedList.query.filter_by(token=jwt_payload["jti"]).first()
     if isinstance(tokenBlocked, TokenBlockedList):
         return True
     else:
         return False
 
-#Conofiguracion de BCrypt
+
+# Conofiguracion de BCrypt
 bcrypt = Bcrypt(app)
 
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url.replace("postgres://", "postgresql://")
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db, compare_type = True)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 
 # Allow CORS requests to this API
@@ -62,31 +72,33 @@ setup_admin(app)
 setup_commands(app)
 
 # Add all endpoints form the API with a "api" prefix
-app.register_blueprint(api, url_prefix='/api')
+app.register_blueprint(api, url_prefix="/api")
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
 # generate sitemap with all your endpoints
-@app.route('/')
+@app.route("/")
 def sitemap():
     if ENV == "development":
         return generate_sitemap(app)
-    return send_from_directory(static_file_dir, 'index.html')
+    return send_from_directory(static_file_dir, "index.html")
+
 
 # any other endpoint will try to serve it like a static file
-@app.route('/<path:path>', methods=['GET'])
+@app.route("/<path:path>", methods=["GET"])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
-        path = 'index.html'
+        path = "index.html"
     response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0 # avoid cache memory
+    response.cache_control.max_age = 0  # avoid cache memory
     return response
 
 
 # this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+if __name__ == "__main__":
+    PORT = int(os.environ.get("PORT", 3001))
+    app.run(host="0.0.0.0", port=PORT, debug=True)
